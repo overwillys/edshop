c96709d0f154247f754d95898629790d24d7c3a4
x3lmorgan <williamstuate4@gmail.com>
2023-12-24 22:03:27 -0300

    modulo 1 listo

--- edshop/settings.py
+++ edshop/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'web',
+    'django_admin_tailwind',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -74,9 +77,13 @@ WSGI_APPLICATION = 'edshop.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "db_edshop",
+        "USER": "root",
+        "PASSWORD": "",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
 
@@ -121,3 +128,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL  =  '/media/'
+
--- web/models.py
+++ web/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    nombre  = models.CharField(max_length=200)
+    fecha_registro = models.DateField(auto_now_add=True) #se pone la fecha automaticamente
+
+    def __str__(self): # cuando instancie un objeto de la clase categoria
+        return self.nombre
+        
+
+class Producto(models.Model):
+    categoria = models.ForeignKey(Categoria, on_delete=models.RESTRICT)   # de uno a muchos tengo que relacionar con la tabla categoria. El restric es para que no borre categoria, si ya tiene productos creados
+    nombre = models.CharField(max_length=200)
+    descripcion = models.TextField(null=True)
+    precio = models.DecimalField(max_digits=9, decimal_places=2)
+    fecha_registro = models.DateField(auto_now_add=True)
+    imagen = models.ImageField(upload_to='productos', blank=True)
+
+    def __str__(self):
+        return self.nombre
